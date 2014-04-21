74141eeb3c88f15883d7f667c8332551be07c9e1
Luke Imhoff <luke_imhoff@rapid7.com>
2014-04-21 09:23:31 -0500

    bundle gem metasploit-erd --test
    MSP-9814

---
+++ lib/metasploit/erd.rb
@@ -0,0 +1,7 @@
+require "metasploit/erd/version"
+
+module Metasploit
+  module Erd
+    # Your code goes here...
+  end
+end
---
+++ lib/metasploit/erd/version.rb
@@ -0,0 +1,5 @@
+module Metasploit
+  module Erd
+    VERSION = "0.0.1"
+  end
+end
